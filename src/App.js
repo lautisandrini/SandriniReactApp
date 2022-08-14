7d0c3b43eac7841e3ac536304b2302ae4c2395c7
Lauti Sandrini <sandrini.it@gmail.com>
2022-08-14 19:22:26 -0300

    empezando mi app en react

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Sandrini Lautaro ReactJS App
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/lautisandrini"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          My GitHub page
         </a>
       </header>
     </div>
