8cb8c5fd2b53e47919ba66bb4f5f02f50c488fa3
Lauti Sandrini <sandrini.it@gmail.com>
2022-08-20 01:53:59 -0300

    creando menu Nav

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
+import NavBar from "./componentes/NavBar";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Sandrini Lautaro ReactJS App
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/lautisandrini"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My GitHub page
-        </a>
-      </header>
+    <div>
+      <NavBar/>
     </div>
   );
 }
