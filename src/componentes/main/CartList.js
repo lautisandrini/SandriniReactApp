8c9e54701bddc490b49301d4fa9cb22513a9760c
Lauti Sandrini <sandrini.it@gmail.com>
2022-09-19 10:35:15 -0300

    agrego CartList y CartEmpty

---
+++ src/componentes/main/CartList.js
@@ -0,0 +1,36 @@
+import { useContext } from "react"
+import {CartContext} from "../../context/CartContext"
+import {BsFillTrashFill} from'react-icons/bs'
+
+
+const CartList = () => {
+
+    const {cart, precioTotal, btnVaciar, removeItem} =useContext(CartContext)
+
+    return(
+        <div>
+            <h2 className="subtitulo">Carrito de compras:</h2>
+            <hr/>
+            <h3 className="detailPrecio2">Precio total: ${precioTotal()}</h3>
+            <h3 className="detailName2" > - Productos seleccionados:</h3>
+            <hr/>
+            { cart.map( (item) => (
+                // aca armar un componenete CarItem mas presentable
+                <div className="cartElement" key={item.id}>
+                    <h4 className="detailName2">{item.nombre} :</h4>
+                    <p className="detailPrecio">Precio: ${item.precio}</p>
+                    <p className="detailStock">Cantidad: {item.cantidad}</p>
+
+                    <button className="btnRemove2" onClick={()=> removeItem(item.id)}><BsFillTrashFill className="btnRemove btn btn-danger"/> Eliminar producto</button>
+                    <hr/>
+                </div>
+            ) )}
+
+            <div className="vaciarContainer">
+                <button onClick={btnVaciar} className="btn btn-danger btnVaciar">Vaciar Carrito</button>
+            </div>
+        </div>
+    )
+}
+
+export default CartList
--- src/componentes/main/EmptyCart.js
+++ src/componentes/main/EmptyCart.js
@@ -1,8 +1,14 @@
 
+import {Link} from 'react-router-dom';
+
 const EmptyCart = () => {
 
     return(
-
+        <div className="vaciarContainer">
+            <h2 className="subtitulo">¡El carrito esta vacio!</h2>
+            <hr/>
+            <Link to="/" className="btn btn-danger btnVaciar">Ver Productos</Link>
+        </div>
     )
 }
 
